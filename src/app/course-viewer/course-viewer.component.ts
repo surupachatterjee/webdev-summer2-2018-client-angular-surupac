30f0ded67fa6f7c11434e71bc788011967770f66
chatterjeesurupa <chatterjee.su@husky.neu.edu>
2018-08-08 00:30:17 -0400

    changes to course viewer

--- src/app/course-viewer/course-viewer.component.ts
+++ src/app/course-viewer/course-viewer.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import {ActivatedRoute} from "@angular/router";
+import {ActivatedRoute, Router} from "@angular/router";
 import {CourseServiceClient} from "../services/course.service.client";
 import {Course} from "../models/course.model.client";
+import {UserServiceClient} from "../services/user.service.client";
 
 @Component({
   selector: 'app-course-viewer',
@@ -10,17 +11,46 @@ import {Course} from "../models/course.model.client";
 })
 export class CourseViewerComponent implements OnInit {
 
-  constructor(private  service : CourseServiceClient,private route:ActivatedRoute) {
+  constructor(private  service : CourseServiceClient,
+              private userService:UserServiceClient,
+              private route :ActivatedRoute,
+              private router :Router) {
     this.route.params.subscribe(params => this.loadCourse(params['courseId']));
   }
 
   course: Course = new Course();
+  username;
+  password;
+  admin = false;
+  loginVal = false;
 
   loadCourse(courseId){
     this.service.findCourseById(courseId)
       .then(course => this.course = course);
   }
+
+  logout() {
+    this.userService
+      .logout()
+      .then(() =>
+        this.router.navigate(['login']));
+
+  }
+
   ngOnInit() {
+    this.userService
+      .profile()
+      .then((user) => {
+          console.log('logged in as : ', user);
+          this.username = user.username;
+          if (this.username !== 'No session maintained') {
+            this.loginVal = true;
+          }
+          if (this.username === 'admin') {
+            this.admin = true;
+          }
+        }
+      );
   }
 
 }
