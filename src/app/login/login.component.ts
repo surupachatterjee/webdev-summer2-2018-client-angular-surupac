906cf9fd7387b909ac831e9908e474c731082f74
tanmoy roy <chatterjee.surupa63@gmail.com>
2018-08-08 13:12:28 -0500

    login validations added

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -12,22 +12,33 @@ export class LoginComponent implements OnInit {
   constructor(private router : Router,
               private service: UserServiceClient) { }
 
-  username;
-  password;
+  username ='';
+  password= '';
 
   login(username,password){
+    if(username === ''){
+      alert("Enter valid username");
+    }else if(password === ''){
+      alert("Please enter your password");
+    }else{
     const user={
       username:username,
       password:password
     };
     this.service.login(user)
       .then((user) =>{
-      if(user.username !== 'No session maintained'){
+      console.log("user that came back" + user.username);
+      if(user.username === 'Invalid credentials'){
+        alert("Invalid user ");
+        this.router.navigate(['login']);
+      }
+      else if(user.username !== 'No session maintained'){
         console.log(user.username);
         this.router.navigate(['profile']);
       }
 
-      });
+      })}
+    ;
 
 
 
